6806a71b7e42a7eded72e0e5809a675e042b7855
Cesar Tamayo <cesar.tamayo@intel.com>
2025-06-25 14:44:56 -0700

    Add convolution to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 source /opt/intel/oneapi/setvars.sh
-dpcpp matrix-multiplication.cpp -o test.bin && ./test.bin
+# dpcpp matrix-multiplication.cpp -o test.bin && ./test.bin
+
+dpcpp -I/usr/include/opencv4 -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_imgproc convolution.cpp -o convolution.bin
+
+./convolution.bin
